bf01ad7bf5b0a03fd8e96157b65da09b9b8174a3
vanshverma721 <vvv.721781@gmail.com>
2024-08-13 15:56:09 +0530

    last commit

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-    console.log("Connected");
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
+    })
+    
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    })
 })
 
 app.get('/', (req, res) => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,40 @@
 const socket = io();
-console.log("hey");
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude });
+    }, (error) => {
+        console.log(error);
+    }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    });
+}
+
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
+
+    if (markers[id]) {
+        // Move the marker to the new location by setting longitude and latitude
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        // Create a new marker at the initial location
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+})
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
