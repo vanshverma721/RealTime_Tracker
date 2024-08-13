a0ee351d5d8a0fecd92e479f78c1bf224c952165
vanshverma721 <vvv.721781@gmail.com>
2024-08-13 13:28:50 +0530

    added view engine and static file setup

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
+
 const socketio = require('socket.io');
-
 const server = http.createServer(app);
-
 const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-app.listen(3000);
+server.listen(3000);
