406038f7fbdc09b5ead728023854f1af73a76163
vanshverma721 <vvv.721781@gmail.com>
2024-08-13 13:22:22 +0530

    socket io template set up

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.listen(3000);
